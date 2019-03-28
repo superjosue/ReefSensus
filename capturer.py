fd821d525b6072a2d0a6d02a438e45754a87c485
superjosue <superjosue@gmail.com>
2019-03-28 19:49:07 -0400

    Revert "subiendo modelo AI"
    This reverts commit c2e378691b7ec7dd1a31a1aab9b5ac23193cea41.

--- capturer.py
+++ capturer.py
@@ -1,17 +1,13 @@
 import streamlink
 import cv2
 import time
-import datetime as dt
-
-
-
+import datetime as dt 
 test ="https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
 url2= "https://www.youtube.com/watch?v=ifqYuTn_6MI"  #explorer North Carolina
 url1 = 'https://www.youtube.com/watch?v=PbzrnUW70gU' #deerfield
 timeSpam=dt.datetime.now()
 streams = streamlink.streams(test)
 cap = cv2.VideoCapture(streams["best"].url)
-
 i=0
 while True:
     time.sleep(1)
@@ -21,6 +17,4 @@ while True:
         print("saving Photo%d.jpg" % i )
         i=i+1
     print("nothing to Save")
-
-
 
